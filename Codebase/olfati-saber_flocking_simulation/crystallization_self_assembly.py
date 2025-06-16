1645587fe4b15985f226e81c4719d08a32fe99ef
Humzah Durrani <hhd8@bu.edu>
2025-06-16 14:19:55 -0400

    Create new sim

---
+++ Codebase/olfati-saber_flocking_simulation/crystallization_self_assembly.py
@@ -0,0 +1,12 @@
+#######################################################################
+# Its about to get wicked up in here:
+# Crystallization Self-Assembly Algorithm (See Sources)
+# 
+#
+# Author: Humzah Durrani
+# STATUS: In-Progress
+# To do: Boundary Configuration, Temperature Dynamic, Obstacles?, 3-D?   
+# 
+# Sources/References:
+#
+#######################################################################
